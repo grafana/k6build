97c54c706741934351cbc0cf62f752c165efc4cc
Pablo Chacin <pablochacin@gmail.com>
2025-01-10 20:14:18 +0100

    use global metrics
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- pkg/builder/builder.go
+++ pkg/builder/builder.go
@@ -31,6 +31,16 @@ const (
 	metricsNamespace = "k6build"
 )
 
+// FIXME: this is a temporary solution to avoid a problem re-registering the metrics
+// when running tests
+func init() {
+	var err error
+	buildMetrics, err = newMetrics()
+	if err != nil {
+		panic(fmt.Sprintf("error creating metrics: %v", err))
+	}
+}
+
 var (
 	ErrAccessingArtifact     = errors.New("accessing artifact")                      //nolint:revive
 	ErrBuildingArtifact      = errors.New("building artifact")                       //nolint:revive
@@ -39,6 +49,8 @@ var (
 	ErrBuildSemverNotAllowed = errors.New("semvers with build metadata not allowed") //nolint:revive
 
 	constrainRe = regexp.MustCompile(opRe + verRe + buildRe)
+
+	buildMetrics *metrics //nolint:gochecknoglobals
 )
 
 // GoOpts defines the options for the go build environment
@@ -163,17 +175,12 @@ func New(ctx context.Context, config Config) (*Builder, error) {
 		return nil, k6build.NewWrappedError(ErrInitializingBuilder, err)
 	}
 
-	metrics, err := newMetrics()
-	if err != nil {
-		return nil, k6build.NewWrappedError(ErrInitializingBuilder, err)
-	}
-
 	return &Builder{
 		allowBuildSemvers: config.Opts.AllowBuildSemvers,
 		catalog:           config.Catalog,
 		builder:           builder,
 		store:             config.Store,
-		metrics:           metrics,
+		metrics:           buildMetrics, // FIXME: each builder should have its own metrics
 	}, nil
 }
 

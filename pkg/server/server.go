a8589bb73110c375d4a29058c3ad0cadf21b7b0a
Lukasz Gut <blinku.dev@gmail.com>
2025-08-22 15:51:26 +0200

    Remove nolint:contextcheck

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -73,7 +73,7 @@ func (a *APIServer) Build(w http.ResponseWriter, r *http.Request) {
 
 	a.log.Debug("processing", "request", req.String())
 
-	artifact, err := a.srv.Build( //nolint:contextcheck
+	artifact, err := a.srv.Build(
 		r.Context(),
 		req.Platform,
 		req.K6Constrains,
@@ -120,7 +120,7 @@ func (a *APIServer) Resolve(w http.ResponseWriter, r *http.Request) {
 
 	a.log.Debug("processing", "request", req.String())
 
-	deps, err := a.srv.Resolve( //nolint:contextcheck
+	deps, err := a.srv.Resolve(
 		r.Context(),
 		req.K6Constrains,
 		req.Dependencies,
